7a2cfb988b6a363289a5860b5b4f6e509a24f4e1
Robin Long <binlon@amazon.com>
2022-06-21 15:32:03 +0800

    update prepareCloudIDE scripts

--- prepareCloud9IDE.sh
+++ prepareCloud9IDE.sh
@@ -38,7 +38,7 @@ echo "export EKS_KEY_ARN=\"$CF_OUT_EKSKeyArn\"" >> ~/.bashrc
 source ~/.bashrc
 
 # eksctl
-echo "install eksctl"
+echo "Install eksctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
@@ -51,12 +51,12 @@ source ~/.bashrc
 
 
 # 辅助工具
-echo "install libs"
+echo "Install jq, envsubst (from GNU gettext utilities) and bash-completion"
 sudo yum -y install jq gettext bash-completion moreutils
 
 
 # 更新 awscli 并配置自动完成
-echo "upgrade awscli to v2"
+echo "Upgrade awscli to v2"
 mv /bin/aws /bin/aws1
 mv ~/anaconda3/bin/aws ~/anaconda3/bin/aws1
 ls -l /usr/local/bin/aws
@@ -73,7 +73,7 @@ aws --version
 
 
 # 安装 kubectl 并配置自动完成
-echo "install kubectl"
+echo "Install kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 cat >> ~/.bashrc <<EOF
@@ -83,11 +83,20 @@ complete -F __start_kubectl k
 EOF
 source ~/.bashrc
 kubectl version --client
+# Enable some kubernetes aliases
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+sudo curl https://raw.githubusercontent.com/blendle/kns/master/bin/kns -o /usr/local/bin/kns && sudo chmod +x $_
+sudo curl https://raw.githubusercontent.com/blendle/kns/master/bin/ktx -o /usr/local/bin/ktx && sudo chmod +x $_
+# echo "alias kgn='kubectl get nodes -L beta.kubernetes.io/arch -L eks.amazonaws.com/capacityType -L beta.kubernetes.io/instance-type -L eks.amazonaws.com/nodegroup -L topology.kubernetes.io/zone -L karpenter.sh/provisioner-name -L karpenter.sh/capacity-type'" | tee -a ~/.bashrc
+echo "alias kgn='kubectl get nodes -L beta.kubernetes.io/arch -L eks.amazonaws.com/capacityType -L beta.kubernetes.io/instance-type -L eks.amazonaws.com/nodegroup -L topology.kubernetes.io/zone'" | tee -a ~/.bashrc
+echo "alias kgnk='kubectl get nodes -L beta.kubernetes.io/arch -L eks.amazonaws.com/capacityType -L beta.kubernetes.io/instance-type -L eks.amazonaws.com/nodegroup -L topology.kubernetes.io/zone -L karpenter.sh/provisioner-name -L karpenter.sh/capacity-type'" | tee -a ~/.bashrc
+source ~/.bashrc
 
 
 
 # 安装 helm
-echo "install helm"
+echo "Install helm"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
@@ -97,7 +106,7 @@ helm repo add stable https://charts.helm.sh/stable
 
 
 # 安装 awscurl 工具 https://github.com/okigan/awscurl
-echo "install awscurl"
+echo "Install awscurl"
 cat >> ~/.bashrc <<EOF
 export PATH=\$PATH:\$HOME/.local/bin:\$HOME/bin:/usr/local/bin
 EOF
@@ -107,12 +116,31 @@ sudo python3 -m pip install awscurl
 
 
 # 安装 session-manager 插件
-echo "install session-manager"
+echo "Install session-manager"
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
 
 sudo yum install -y session-manager-plugin.rpm
 
 session-manager-plugin
+
+
+# More tools
+echo "Install yq for yaml processing"
+echo 'yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}' | tee -a ~/.bashrc && source ~/.bashrc
+
+
+echo "Install c9 to open files in cloud9"
+npm install -g c9
+# example  c9 open ~/package.json
+
+
+echo "Install k9s a Kubernetes CLI To Manage Your Clusters In Style"
+curl -sS https://webinstall.dev/k9s | bash
+# 参考 https://segmentfault.com/a/1190000039755239
+
+
 
 # 最后再执行一次 source
 echo "source .bashrc"
