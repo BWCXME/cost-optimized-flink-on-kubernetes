98ba5b0753ded8d4fb6dc5c6968281688b42ac99
Robin Long <binlon@amazon.com>
2022-07-27 16:48:07 +0800

    add a couple of tools

--- prepareCloud9IDE.sh
+++ prepareCloud9IDE.sh
@@ -1,3 +1,6 @@
+# 参考
+# https://github.com/fmmasood/eks-cli-init-tools/blob/main/cli_tools.sh
+
 if [ $# -eq 0 ]
   then
     echo "Please provide CloudformationStackName"
@@ -46,10 +49,11 @@ echo "==============================================="
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
-
 # 配置自动完成
 cat >> ~/.bashrc <<EOF
 . <(eksctl completion bash)
+alias e=eksctl
+complete -F __start_eksctl e
 EOF
 source ~/.bashrc
 
@@ -80,10 +84,17 @@ source ~/.bashrc
 aws --version
 
 
-# 安装 kubectl 并配置自动完成
+echo "==============================================="
+echo "  Config Cloud9 ......"
+echo "==============================================="
+#aws cloud9 update-environment --environment-id $C9_PID --managed-credentials-action DISABLE
+rm -vf ${HOME}/.aws/credentials
+
+
 echo "==============================================="
 echo "  Install kubectl ......"
 echo "==============================================="
+# 安装 kubectl 并配置自动完成
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 cat >> ~/.bashrc <<EOF
@@ -196,6 +207,19 @@ echo "==============================================="
 # https://medium.doit-intl.com/kubernetes-how-to-automatically-detect-and-deal-with-deprecated-apis-f9a8fc23444c
 sh -c "$(curl -sSL https://git.io/install-kubent)"
 
+
+echo "==============================================="
+echo "  Install IAM Authenticator ......"
+echo "==============================================="
+# curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
+# curl -o aws-iam-authenticator https://s3.us-west-2.amazonaws.com/amazon-eks/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator
+curl -o aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.5.9/aws-iam-authenticator_0.5.9_linux_amd64
+chmod +x ./aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+source ~/.bashrc
+
+
 echo "==============================================="
 echo "  Install Maven ......"
 echo "==============================================="
@@ -206,6 +230,82 @@ export PATH="/opt/apache-maven-3.8.6/bin:$PATH"
 EOF
 source ~/.bashrc
 mvn --version
+
+
+echo "==============================================="
+echo "  Config Go ......"
+echo "==============================================="
+go version
+export GOPATH=$(go env GOPATH)
+echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
+source ~/.bashrc
+
+
+echo "==============================================="
+echo "  Install ccat ......"
+echo "==============================================="
+go install github.com/owenthereal/ccat@latest
+cat >> ~/.bashrc <<EOF
+alias cat=ccat
+EOF
+source ~/.bashrc
+
+
+echo "==============================================="
+echo "  Install kubescape ......"
+echo "==============================================="
+curl -s https://raw.githubusercontent.com/armosec/kubescape/master/install.sh | /bin/bash
+
+
+echo "==============================================="
+echo "  Install ec2-instance-selector ......"
+echo "==============================================="
+curl -Lo ec2-instance-selector https://github.com/aws/amazon-ec2-instance-selector/releases/download/v2.3.3/ec2-instance-selector-`uname | tr '[:upper:]' '[:lower:]'`-amd64 && chmod +x ec2-instance-selector
+chmod +x ./ec2-instance-selector
+mkdir -p $HOME/bin && mv ./ec2-instance-selector $HOME/bin/ec2-instance-selector
+
+
+echo "==============================================="
+echo "  Install kind ......"
+echo "==============================================="
+curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.14.0/kind-$(uname)-amd64"
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/
+
+
+echo "==============================================="
+echo "  Install Flux CLI ......"
+echo "==============================================="
+curl -s https://fluxcd.io/install.sh | sudo bash
+flux --version
+
+
+echo "==============================================="
+echo "  Install siege ......"
+echo "==============================================="
+sudo yum install siege -y
+siege -V
+#siege -q -t 15S -c 200 -i URL
+#ab -c 500 -n 30000 http://$(kubectl get ing -n front-end --output=json | jq -r .items[].status.loadBalancer.ingress[].hostname)/
+
+
+echo "==============================================="
+echo "  Install terraform ......"
+echo "==============================================="
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum install terraform -y
+terraform --version
+
+
+echo "==============================================="
+echo "  More Aliases ......"
+echo "==============================================="
+cat >> ~/.bashrc <<EOF
+alias c=clear
+EOF
+source ~/.bashrc
+
 
 # 最后再执行一次 source
 echo "source .bashrc"
