09bc5e13362d33abb62f1ae700b5d1cf96bcc88c
Robin Long <binlon@amazon.com>
2022-07-28 12:27:59 +0800

    update cfn

--- prepareCloud9IDE.sh
+++ prepareCloud9IDE.sh
@@ -1,13 +1,6 @@
 # 参考
 # https://github.com/fmmasood/eks-cli-init-tools/blob/main/cli_tools.sh
 
-if [ $# -eq 0 ]
-  then
-    echo "Please provide CloudformationStackName"
-    return
-fi
-
-# 配置环境变量，方便后续操作
 echo "==============================================="
 echo "  Config envs ......"
 echo "==============================================="
@@ -26,21 +19,6 @@ aws configure set region $AWS_REGION
 
 source ~/.bashrc
 aws sts get-caller-identity
-
-# 将CloudFormation的Output保存到环境变量，然后进一步记录到 `.bashrc`
-export $(aws cloudformation describe-stacks --stack-name $1 --output text --query 'Stacks[0].Outputs[].join(`=`, [join(`_`, [`CF`, `OUT`, OutputKey]), OutputValue ])' --region $AWS_REGION)
-echo "export EKS_VPC_ID=\"$CF_OUT_VpcId\"" >> ~/.bashrc
-echo "export EKS_CONTROLPLANE_SG=\"$CF_OUT_ControlPlaneSecurityGroup\"" >> ~/.bashrc
-echo "export EKS_SHAREDNODE_SG=\"$CF_OUT_SharedNodeSecurityGroup\"" >> ~/.bashrc
-echo "export EKS_PUB_SUBNET_01=\"$CF_OUT_PublicSubnet1\"" >> ~/.bashrc
-echo "export EKS_PUB_SUBNET_02=\"$CF_OUT_PublicSubnet2\"" >> ~/.bashrc
-echo "export EKS_PUB_SUBNET_03=\"$CF_OUT_PublicSubnet3\"" >> ~/.bashrc
-echo "export EKS_PRI_SUBNET_01=\"$CF_OUT_PrivateSubnet1\"" >> ~/.bashrc
-echo "export EKS_PRI_SUBNET_02=\"$CF_OUT_PrivateSubnet2\"" >> ~/.bashrc
-echo "export EKS_PRI_SUBNET_03=\"$CF_OUT_PrivateSubnet3\"" >> ~/.bashrc
-echo "export EKS_KEY_ARN=\"$CF_OUT_EKSKeyArn\"" >> ~/.bashrc
-
-source ~/.bashrc
 
 # eksctl
 echo "==============================================="
